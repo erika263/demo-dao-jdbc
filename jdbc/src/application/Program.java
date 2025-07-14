07a4d5406f04c484e946856edf731ca3b2ada3f7
Érika Cristina Castro <erika.contax@gmail.com>
2025-07-14 08:44:27 -0300

    findALL implementation

--- jdbc/src/application/Program.java
+++ jdbc/src/application/Program.java
@@ -24,6 +24,15 @@ public class Program {
         for(Seller obj : list) {
         	System.out.println(obj);
         }
+        
+        System.out.println(seller);
+        System.out.println("\n === TEST 3: seller findALL =====");
+         list = sellerDao.findAll();
+       
+        for(Seller obj : list) {
+        	System.out.println(obj);
+        }
 	}
+	
 
 }
